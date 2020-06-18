0c5ef27dba5519fe66d9f260115295679f34d0a4
Julio Gonçalves <juliorogo.jc@gmail.com>
2020-06-17 23:33:26 -0300

    Criando componente para os cards

---
+++ src/components/shared/Card.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="row" :class="margin">
+		<div :class="formatCol">
+			<div class="card">
+				<div class="card-body">
+					<slot></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		margin: String,
+		col: Number,
+	},
+	computed: {
+		formatCol: function () {
+			return `col-${ this.col }`;
+		}
+	}
+}
+</script>
+
+<style>
+</style>
--- src/views/movie/New.vue
+++ src/views/movie/New.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <p>Cadastro de filmes</p>
-  </div>
+  <my-card margin="mt-4 ml-1 mr-1" col="12">
+    <h5 class="card-title">Cadastro de filmes</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </my-card>
 </template>
 
 <script>
-export default {
 
+import Card from '../../components/shared/Card';
+
+export default {
+  components: {
+    'my-card' : Card
+  }
 }
 </script>
 
